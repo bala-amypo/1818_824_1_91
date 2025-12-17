4a119edfcca61327e66346aa0c9cecb16371dc31
bala-amypo <bala@amypo.in>
2025-12-17 09:45:51 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,4 +1,5 @@
 package com.example.demo.model;
+
 import jakarta.persistence.Entity;
 import jakarta.persistence.Id;
 import jakarta.persistence.PrePersist;
@@ -6,7 +7,7 @@ import jakarta.persistence.GeneratedValue;
 import jakarta.validation.constraints.NotNull;
 import jakarta.validation.constraints.Size;
 @Entity
-class CategorizationRule{
+public class CategorizationRule{
     @Id
     private Long id;
     private String category;
