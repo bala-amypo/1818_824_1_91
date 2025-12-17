b1f4cea3408d6cfcbc28213e83bda9cd72b004d3
bala-amypo <bala@amypo.in>
2025-12-17 06:47:43 +0000

    User commit

--- src/main/java/com/example/demo/model/UrgencyPolicy.java
+++ src/main/java/com/example/demo/model/UrgencyPolicy.java
@@ -1,5 +1,15 @@
 package com.example.demo.model;
-
+import jakarta.persistance.Entity;
+@Entity
 public class UrgencyPolicy{
-    
+    @Id
+    private Long id;
+    private String category;
+    private 
+    public getId(){
+
+    }
+    public setId(){
+
+    }
 }
