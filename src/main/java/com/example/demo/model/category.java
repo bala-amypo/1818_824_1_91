f53fa1bf621f4910d96ee67c959b90c10785a7cb
bala-amypo <bala@amypo.in>
2025-12-17 05:27:43 +0000

    User commit

--- src/main/java/com/example/demo/model/category.java
+++ src/main/java/com/example/demo/model/category.java
@@ -2,5 +2,14 @@ package com.example.demo;
 import jakarta.persistance.Entity;
 @Entity
 public class category{
-    private id
+    private Long id;
+    private String categoryName;
+    private String description;
+    private String defaultUrgency;
+    private String createdAt;
+    private String createdAt;
+    public void getId(Long id){
+        this.id=id;
+    }
+    public 
 }
