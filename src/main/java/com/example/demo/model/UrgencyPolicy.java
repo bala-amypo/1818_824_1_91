bde2c4d3b4d01fb62d9c7fbafdc073c5a7b95ee1
bala-amypo <bala@amypo.in>
2025-12-17 18:55:55 +0000

    User commit

--- src/main/java/com/example/demo/model/UrgencyPolicy.java
+++ src/main/java/com/example/demo/model/UrgencyPolicy.java
@@ -1,42 +1,45 @@
-// package com.example.demo.model;
-// import jakarta.persistance.Entity;
-// @Entity
-// public class UrgencyPolicy{
-//     @Id
-//     private Long id;
-//     private String category;
-//     private String keyword;
-//     private String matchType;
-//     private Long priority;
-//     private String createdAt;
-//     public Long getId(){
-//         return id;
-//     }
-//     public void setId(Long id){
-//         this.id=id;
-//     }
-//     public String getCategory(){
-//         return category;
-//     }
-//     public void setCategory(String category){
-//         this.category=category;
-//     }
-//     public String getMatchType(){
-//         return matchType;
-//     } 
-//     public void setMatchType(String matchType){
-//         this.matchType=matchType;
-//     }
-//     public String getPriority(){
-//         return priority;
-//     }
-//     public void setPriority(String priority){
-//         this.priority=priority;
-//     }
-//     public void setCreatedAt(String createdAt){
-//         this.createdAt=createdAt
-//     }
-//     public String getCreatedAt(){
-//         return createdAt;
-//     }
-// }
+package com.example.demo.model;
+import jakarta.persistence.Entity;
+import jakarta.persistence.Id;
+import org.hibernate.annotations.CreationTimestamp;
+import j
+@Entity
+public class UrgencyPolicy{
+    @Id
+    private Long id;
+    private String category;
+    private String keyword;
+    private String matchType;
+    private Long priority;
+    private  createdAt;
+    public Long getId(){
+        return id;
+    }
+    public void setId(Long id){
+        this.id=id;
+    }
+    public String getCategory(){
+        return category;
+    }
+    public void setCategory(String category){
+        this.category=category;
+    }
+    public String getMatchType(){
+        return matchType;
+    } 
+    public void setMatchType(String matchType){
+        this.matchType=matchType;
+    }
+    public String getPriority(){
+        return priority;
+    }
+    public void setPriority(String priority){
+        this.priority=priority;
+    }
+    public void setCreatedAt(String createdAt){
+        this.createdAt=createdAt
+    }
+    public String getCreatedAt(){
+        return createdAt;
+    }
+}
