29534ba2416ae4b50beafd38aa227b9157fc2c78
bala-amypo <bala@amypo.in>
2025-12-17 19:10:54 +0000

    User commit

--- src/main/java/com/example/demo/model/User.java
+++ src/main/java/com/example/demo/model/User.java
@@ -1,4 +1,21 @@
-// package com.demo.model
-// public class User{
-
-// }
+package com.demo.model;
+import jakarta.persistence.Id;
+import jakarta.persistence.Entity;
+import org.hibernate.annotations.CreationTimestamp;
+@Entity
+public class User{
+    @Id
+    private Long id;
+    private String fullName;
+    private String email;
+    private String password;
+    private String role;
+    @CreationTimestamp
+    private Timestamp createdAt;
+    Long getId(){
+        return id;
+    }
+    String getFullname(){
+        return fullName;
+    }
+}
