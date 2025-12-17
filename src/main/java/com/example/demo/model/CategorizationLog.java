9603f18f5c7a122515518a652181afe87600e3bd
bala-amypo <bala@amypo.in>
2025-12-17 02:19:49 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationLog.java
+++ src/main/java/com/example/demo/model/CategorizationLog.java
@@ -1,4 +1,11 @@
 package com.example.demo
 public class CategorizationLog{
-    private L
+    private Long id;
+    private String Ticket;
+    private String appliedRule;
+    private String MatchedKeyword;
+    private String assignedCategory;
+    private String assignedUrgency;
+    private String loggedAt;
+    p
 }
