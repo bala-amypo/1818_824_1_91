a0b34986556479df364c651f14f735ebfd5db978
bala-amypo <bala@amypo.in>
2025-12-17 09:05:51 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -12,15 +12,15 @@ class CategorizationRule{
     private Long id;
     private String category;
     @NotNull(message="This Feild is Recquired")
-    private String keyword;
+     private String keyword;
     private String matchType;
-    @Size(min=0,message="thepriority is greterthan 0")
+    @Size(min=0,message="the priority is greterthan 0")
     private int priority;
     @GeneratedValue(strategy=GenerationType.Identity)
     private String createdAt;
     public void setCategory(String category){
         this.category=category;
-    }
+        }
     public String getCategory(){
         return category;
     }
@@ -35,27 +35,26 @@ class CategorizationRule{
     }
     public String getMatchType(){
         return matchType;
-    }  
-    public void getPriority(int priority){
-        this.priority=priority;
-    }
-    public int setPriority(){
-        return priority;
-    }
-    public void setCreatedAt(String category){
-        this.category=category;
-    }
-    public String getCreatedAt(){
-        return createdAt;
-    }
-    public CategorizationRule(Long id,String category,String keyword,String matchType,int Priority,String createdAt){
-        this.id=id;
-        this.category=category;
-        this.keyword=keyword;
-        this.matchType=matchType;
-        this.priority=priority;
-        this.createdAt=createdAt;
-    }
-    public CategorizationRule(){}
-    CategorizationRule st=new CategorizationRule();
-}
+                                                                                                                                        }  
+                                                                                                                                            public void getPriority(int priority){
+                                                                                                                                                    this.priority=priority;
+                                                                                                                                                        }
+                                                                                                                                                            public int setPriority(){
+                                                                                                                                                                    return priority;
+                                                                                                                                                                        }
+                                                                                                                                                                            public void setCreatedAt(String category){
+                                                                                                                                                                                    this.category=category;
+                                                                                                                                                                                        }
+                                                                                                                                                                                            public String getCreatedAt(){
+                                                                                                                                                                                                    return createdAt;
+                                                                                                                                                                                                        }
+                                                                                                                                                                                                            public CategorizationRule(Long id,String category,String keyword,String matchType,int Priority,String createdAt){
+                                                                                                                                                                                                                    this.id=id;
+                                                                                                                                                                                                                            this.category=category;
+                                                                                                                                                                                                                                    this.keyword=keyword;
+                                                                                                                                                                                                                                            this.matchType=matchType;
+                                                                                                                                                                                                                                                    this.priority=priority;
+                                                                                                                                                                                                                                                            this.createdAt=createdAt;
+                                                                                                                                                                                                                                                                }
+                                                                                                                                                                                                                                                                    public CategorizationRule(){}
+                                                                                                                                                                                                                                                                        CategorizationRule st=new CategorizationRule
