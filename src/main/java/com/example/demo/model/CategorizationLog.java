b4ab50036b6b35526272b1748939395b18785bd3
bala-amypo <bala@amypo.in>
2025-12-17 02:35:15 +0000

    User commit

--- src/main/java/com/example/demo/model/CategorizationLog.java
+++ src/main/java/com/example/demo/model/CategorizationLog.java
@@ -7,5 +7,41 @@ public class CategorizationLog{
     private String assignedCategory;
     private String assignedUrgency;
     private String loggedAt;
-    p
+    public void getTicket(){
+        this.Ticket=Ticket;
+    }
+    public String setTicket(String Ticket){
+        return Ticket;
+    }
+    public void getAppliedRule(){
+        this.appliedRule=apppliedRule;
+    }
+    public String setAppliedRule(String appliedRule){
+        return appliedRule;
+    }
+    public void getMatchedKeyword(){
+        this.matchedKeyword=matchedKeyword;
+    }
+    public String setTicket(String matchedKeyword){
+        return matchedKeyword;
+    }
+    public void getAssignedCategory(){
+        this.assignedCategory=assignedCategory;
+    }
+    public String setAssignedCategory(String assignedCategory){
+        return assignedCtaegory;
+    }
+    public void getAssignedUrgency(){
+        this.assignedUrgency=assignedUrgency;
+    }
+    public String setAssignedUrgency(String assignedUrgency){
+        return assignedUrgency;
+    }
+    public void getLoggedAt(){
+        this.loggedAt=loggedAt;
+    }
+    public String setLoggedAt(String loggedAt){
+        return loggedAt;
+    }
+    
 }
--- src/main/java/com/example/demo/model/CategorizationRule.java
+++ src/main/java/com/example/demo/model/CategorizationRule.java
@@ -1,4 +1,21 @@
 package com.example.demo
 class CategorizationRule{
-
+    private Long int;
+    private String category;
+    private String keyword;
+    private String matchType;
+    private int priority;
+    private String createdAt;
+    public void setCategory(){
+        this.category=category;
+    }
+    public String getCategory(String GetCategory){
+        return getCategory;
+    }
+    public void setKeyword(){
+        this.keyword=category;
+    }
+    public String getCategory(String GetCategory){
+        return getCategory;
+    }  
 }
