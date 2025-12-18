f8ef4212683a3c86d65eb5cbceb4b0d4c7f090c3
bala-amypo <bala@amypo.in>
2025-12-18 16:08:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -2,7 +2,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.Mapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import com.example.demo.service.CategoryService;
 import com.example.demo.model.Category;
 import java.util.List;
