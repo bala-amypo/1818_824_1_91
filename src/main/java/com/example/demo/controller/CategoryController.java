4a05219672e059d3ebb270e9d40cabb75fd99537
bala-amypo <bala@amypo.in>
2025-12-18 15:59:15 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -1,16 +1,19 @@
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springbiframework.web.nd.annotation.PostMapping;
+import org.springbiframework.web.nd.annotation.Mapping;
 import com.example.demo.service.CategoryService;
 import com.example.demo.model.Category;
 @RestController
 public class CategoryController{
 @Autowired
 CategoryService cs;
+@PostMapping("/postd")
 public Category postData(@RequestBody Category cty){
     return cs.createCategory(cty);
 }
+@
 public List<Category> Alloutput(){
     return cs.getAllCategories();
 }
