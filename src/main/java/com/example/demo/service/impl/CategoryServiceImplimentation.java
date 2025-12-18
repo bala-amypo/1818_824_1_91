cbd73be194caaad82dbd2cdd80a16aaef2b87c38
bala-amypo <bala@amypo.in>
2025-12-18 10:20:22 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategoryServiceImplimentation.java
+++ src/main/java/com/example/demo/service/impl/CategoryServiceImplimentation.java
@@ -3,10 +3,12 @@ import com.example.demo.service.CategoryService;
 import com.example.demo.entity.Category;
 import com.example.demo.repository.Categoryrepo;
 import org.springframework.beans.factory.annotations.Autowired;
+import jakarta.persistence.Service;
+@Service
 class CategoryServiceImplimentation implements CategoryService{
     @Autowired
     Categoryrepo obj;
     public Category createCategory(Category category){
-        return 
+        return obj.save(category);
     }
 }
