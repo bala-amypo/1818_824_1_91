b8479c516b67acef875fb4563900ff1a99482da6
bala-amypo <bala@amypo.in>
2025-12-18 19:02:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/TicketServiceImplementation.java
+++ src/main/java/com/example/demo/service/impl/TicketServiceImplementation.java
@@ -1,16 +1,16 @@
 import org.springframework.web.bind.annotation.Service;
 import com.example.demo.model.Ticket;
 import com.example.demo.repository.Ticketrepo;
-import org.springframework.bean.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Autowired;
 @Service
 class TicketServiceImplementation implements TicketService{
 @Autowired
 Ticketrepo obj;
     public Ticket createTicket(Ticket Ticket){
-        return obj.create(Ticket);
+        return obj.save(Ticket);
     }
     public Ticket getTicket(Long id){
-        return obj.getById(id);
+        return obj.getById(id).orElse(null);
     }
     public List<Category> getAllCategories(){
         return obj.findAll();
