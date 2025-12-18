6d149af2a1c95fd521c7d6b5505c2975bca3c740
bala-amypo <bala@amypo.in>
2025-12-18 18:17:16 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategoryController.java
+++ src/main/java/com/example/demo/controller/CategoryController.java
@@ -4,7 +4,6 @@ import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
 import com.example.demo.service.CategoryService;
 import com.example.demo.model.Category;
@@ -23,6 +22,6 @@ List<Category> getData(){
 }
 @GetMapping("/{id}")
 Category getIdData(@PathVariable Long id){
-    return getCategory(id);
+    return cs.getCategory(id);
 }
 }
