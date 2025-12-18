bd190ddb29343850f6c833955fb639f11c4e8db7
bala-amypo <bala@amypo.in>
2025-12-18 08:25:22 +0000

    User commit

--- src/main/java/com/example/demo/repository/CategorizationLogreop.java
+++ src/main/java/com/example/demo/repository/CategorizationLogreop.java
@@ -1,6 +1,6 @@
 package com.example.demo.repository;
 import com.example.demo.model.CategorizationLog;
 import org.springframework.data.jpa.repository.JpaRepository;
-public interface Categorizationrepo extends JPARepository<CategorizationLog,Long>{
-    
+public interface CategorizationLogrepo extends JpaRepository<CategorizationLog,Long>{
+
 }
