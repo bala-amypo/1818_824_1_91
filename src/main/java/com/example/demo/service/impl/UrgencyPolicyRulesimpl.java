586ad565dbeadca6e27a1cfd446e32a18d1576b7
bala-amypo <bala@amypo.in>
2025-12-19 16:47:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
@@ -1,2 +1,10 @@
 import com.example.demo.model.UrgencyPolicy;
-import org.springframework.web.bind.annotation.;
+import com.example.demo.service.impl.UrgencyPolicyRules;
+class UrgencyPolicyRulesimpl inplementation UrgencyPolicy{
+    public Urgencypolicy createPolicy(){
+
+    }
+    public List<Urgencypolicy> getAllpolicies(){
+    }
+    public Urgencypolicy getPolicy(Long id);
+}
