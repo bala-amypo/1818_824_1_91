5b6ddb1d802a1df9263a0b9b2ab8f6389fc7c537
bala-amypo <bala@amypo.in>
2025-12-19 02:06:53 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -6,7 +6,13 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
+import org.sprungFramework.web.bind.annotation.RequestBody;
 @RestController;
 public class TicketController extends TicketService{
-    Ticket 
+    @Autowired
+    Tickets
+    @PostMapping("/ticket")
+    public Ticket create(@RequestBody Ticket ti){
+
+    } 
 }
