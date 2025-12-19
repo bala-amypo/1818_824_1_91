7b87fe17a2561b657354a1d666365d64f0c14b2e
bala-amypo <bala@amypo.in>
2025-12-19 16:35:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyRules.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyRules.java
@@ -1,6 +1,8 @@
 package com.example.demo.service.impl;
 import com.example.demo.entity.Urgencypolicy;
+import java.util.List;
 interface UrgencyPolicy{
     public Urgencypolicy createPolicy();
-    public Get
+    public List<Urgencypolicy> getAllpolicies();
+    public Urgencypolicy getPolicy(Long id);
 }
