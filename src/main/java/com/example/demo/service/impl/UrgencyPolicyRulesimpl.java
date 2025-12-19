28b4d44e1a092f5ceb4e953af059780ef954238d
bala-amypo <bala@amypo.in>
2025-12-19 17:02:37 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
@@ -2,11 +2,14 @@ import com.example.demo.model.UrgencyPolicy;
 import com.example.demo.service.impl.UrgencyPolicyRules;
 import com.example.demo.service.repository.UrgecyPolicyrepo;
 import org.springframework.sterotype.Service;
-import o
+import org.springframework.beans.factory.annotation.Autowired;
 
 @Service
 class UrgencyPolicyRulesimpl inplementation UrgencyPolicy{
+    @Autowired
+    
     public Urgencypolicy createPolicy(UrgencyPolicy policy){
+        
     }
     public List<Urgencypolicy> getAllpolicies(){
     }
