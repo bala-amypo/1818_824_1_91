fc0af69a566314f5236b2a1e92cbbce0bb2217a0
bala-amypo <bala@amypo.in>
2025-12-19 03:47:01 +0000

    User commit

--- src/main/java/com/example/demo/model/Ticket.java
+++ src/main/java/com/example/demo/model/Ticket.java
@@ -1,68 +1,68 @@
-package com.example.demo.model;
-import jakarta.persistence.Entity;
-import jakarta.persistence.Id;
-import org.hibernate.annotations.CreationTimestamp;
-import java.sql.Timestamp;
-@Entity
-public class Ticket{
-    @Id
-    private Long id;
-    private String fullName;
-    private String location;
-    private String createdBy;
-    @CreationTimestamp
-    private Timestamp createdAt;
-    private String assignedCategory;
-    private String urgencyLevel;
-    public Long getId(){
-        return id;
-    }
-    public void setId(Long id){
-        this.id=id;
-    }
-    public String getFullName(){
-        return fullName;
-    }
-    public void setFullName(){
-        this.fullName=fullName;
-    }
-    public String getLocation(){
-        return location;
-    }
-    public void setLocation(String location){
-        this.location=location;
-    }
-    public String getCreatedBy(){
-        return createdBy;
-    }
-    public void setCreatedBy(String createdBy){
-        this.createdBy=createdBy;
-    }
-    public String getAssignedcategory(){
-        return assignedCategory;
-    }
-    public void setAssignedCtaegory(String assignedCategory){
-        this.assignedCategory=assignedCategory;
-    }
-    public String getUrgencyLevel(){
-        return urgencyLevel;
-    }
-    public void setUrgencyLevel(String urgencyLevel){
-        this.urgencyLevel=urgencyLevel;
-    }
-    public Timestamp getCreatedAt(){
-        return createdAt;
-    }
-    public void setCreatedAt(Timestamp createdAt){
-        this.createdAt=createdAt;
-    }
-    public Ticket(Long id,String fullName,String Loaction,String createdBy,Timestamp createdAt,String assignedCategory,String urgencyLevel){
-        this.id=id;
-        this.location=location;
-        this.createdBy=createdBy;
-        this.createdAt=createdAt;
-        this.assignedCategory=assignedCategory;
-        this.urgencyLevel=urgencyLevel;
-    }
-    public Ticket(){}
-}
+// package com.example.demo.model;
+// import jakarta.persistence.Entity;
+// import jakarta.persistence.Id;
+// import org.hibernate.annotations.CreationTimestamp;
+// import java.sql.Timestamp;
+// @Entity
+// public class Ticket{
+//     @Id
+//     private Long id;
+//     private String fullName;
+//     private String location;
+//     private String createdBy;
+//     @CreationTimestamp
+//     private Timestamp createdAt;
+//     private String assignedCategory;
+//     private String urgencyLevel;
+//     public Long getId(){
+//         return id;
+//     }
+//     public void setId(Long id){
+//         this.id=id;
+//     }
+//     public String getFullName(){
+//         return fullName;
+//     }
+//     public void setFullName(){
+//         this.fullName=fullName;
+//     }
+//     public String getLocation(){
+//         return location;
+//     }
+//     public void setLocation(String location){
+//         this.location=location;
+//     }
+//     public String getCreatedBy(){
+//         return createdBy;
+//     }
+//     public void setCreatedBy(String createdBy){
+//         this.createdBy=createdBy;
+//     }
+//     public String getAssignedcategory(){
+//         return assignedCategory;
+//     }
+//     public void setAssignedCtaegory(String assignedCategory){
+//         this.assignedCategory=assignedCategory;
+//     }
+//     public String getUrgencyLevel(){
+//         return urgencyLevel;
+//     }
+//     public void setUrgencyLevel(String urgencyLevel){
+//         this.urgencyLevel=urgencyLevel;
+//     }
+//     public Timestamp getCreatedAt(){
+//         return createdAt;
+//     }
+//     public void setCreatedAt(Timestamp createdAt){
+//         this.createdAt=createdAt;
+//     }
+//     public Ticket(Long id,String fullName,String Loaction,String createdBy,Timestamp createdAt,String assignedCategory,String urgencyLevel){
+//         this.id=id;
+//         this.location=location;
+//         this.createdBy=createdBy;
+//         this.createdAt=createdAt;
+//         this.assignedCategory=assignedCategory;
+//         this.urgencyLevel=urgencyLevel;
+//     }
+//     public Ticket(){}
+// }
