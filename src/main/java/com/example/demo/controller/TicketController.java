cc01328f8ea7cf3050655426cc43a40b52e72b75
bala-amypo <bala@amypo.in>
2025-12-19 02:03:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -1,6 +1,12 @@
 package com.example.demo.controller;
-import org.springframework.beans.factory.annotation.Autowired;
 import com.example.demo.model.Ticket;
 import com.example.demo.service.impl.TicketService;
+import org.springframework.stereotype.Autowired;
 import java.util.List;
-import org.springframework.
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+@RestController;
+public class TicketController extends TicketService{
+    Ticket 
+}
