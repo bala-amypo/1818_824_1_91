923c3f6a57fea04bedaf0cddc6c90c9982e48882
bala-amypo <bala@amypo.in>
2025-12-19 02:18:56 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -6,7 +6,7 @@ import java.util.List;
 import org.springframework.web.bind.annotation.RestController;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springFramework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RequestBody;
 @RestController
 public class TicketController{
     @Autowired
