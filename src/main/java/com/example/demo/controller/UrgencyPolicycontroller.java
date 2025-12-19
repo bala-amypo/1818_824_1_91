07e125a5caa16961a0b7f2dc25fc82f18ade2d55
bala-amypo <bala@amypo.in>
2025-12-19 17:32:37 +0000

    User commit

--- src/main/java/com/example/demo/controller/UrgencyPolicycontroller.java
+++ src/main/java/com/example/demo/controller/UrgencyPolicycontroller.java
@@ -1,12 +1,12 @@
 import org.srpingframework.web.bind.annotation.RestController;
 import org.srpingframework.web.bind.annotation.RequestBody;
-import org.srpingframework.web.bind.annotation.;
+import java.util.List;
 import com.example.demo.service.UrgencyPolicyService;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.srpingframework.web.bind.annotation.PostMapping;
-import org.springframework.factory.
-class UrgencyPolicycontroller{
-
+import org.springframework.factory.beans.annotations.Autowired;
 @RestController
+class UrgencyPolicycontroller{
+@Autowired
 
 }
