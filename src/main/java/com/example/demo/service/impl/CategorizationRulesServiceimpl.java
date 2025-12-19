06e5214e2ef5034c92fcbcb8b9cd5eb75896cc49
bala-amypo <bala@amypo.in>
2025-12-19 16:01:15 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
@@ -1,6 +1,12 @@
 package com.example.demo.service.impl;
 class CategorizatrionRulesServiceimpl implements CtaegorizationRuleService{
-    public CategorizationRule createRule(Long categoryId,CategorizationRule rule){}
-    public List<CategorizationRule> getRulesByCategory(Long categoryId){}
-    public CategorizationRule getRule(Long id)
+    public CategorizationRule createRule(Long categoryId,CategorizationRule rule){
+        
+    }
+    public List<CategorizationRule> getRulesByCategory(Long categoryId){
+
+    }
+    public CategorizationRule getRule(Long id){
+
+    }
 }
