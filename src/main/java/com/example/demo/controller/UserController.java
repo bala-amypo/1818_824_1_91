417d83ff75cbcb86a3adcd7c35133c0afb03a354
bala-amypo <bala@amypo.in>
2025-12-20 10:14:51 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -2,12 +2,12 @@ package com.example.demo.controller;
 import com.example.model.User;
 import com.example.service.Userservice;
 import org.springframework.beans.factory.annotations.Autowired;
-import org.springframework.web.bind.annotations.RequestBody;
-import org.springframework.web.bind.annotations.RestController;
-import org.springframework.web.bind.annotations.GetMapping;
-import org.springframework.web.bind.annotations.PostMapping;
-import org.springframework.web.bind.annotations.RequestBody;
-import org.springfarmew
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
 import java.util.List;
 
 @RestController
@@ -18,11 +18,12 @@ public class UserController{
     public User Register(@RequestBody User user){
         return obj.register(user);
     }
-    @PostMapping("/{id}")
+    @GetMapping("/{id}")
     public User GetUser(@PathVariable Long id){
         return obj.getUser(id);
     }
-    public User FindByEmail(String  Email){
+    @GetMapping("/{Eemail}")
+    public User FindByEmail(@PathVariable String  Email){
         return obj.findByEmail(Email);
     }
 }
