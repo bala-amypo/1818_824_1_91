e4bec3e6782ee813ed922ceb67c9d46457e117f9
bala-amypo <bala@amypo.in>
2025-12-20 10:26:52 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/Userserviceimpl.java
+++ src/main/java/com/example/demo/service/impl/Userserviceimpl.java
@@ -8,13 +8,13 @@ import org.springframework.stereotype.Service;
 public class Userserviceimpl implements Userservice{
     @Autowired
     Userrepo obj;
-    User register(User user){
+    public User register(User user){
         return obj.save(user);
     }
-    User getUser(Long id){
+    public User getUser(Long id){
         return obj.findById(id);
     }
-    User findByEmail(String  Email){
+    public User findByEmail(String  Email){
         return obj.findByEmail(Email);
     }
 }
