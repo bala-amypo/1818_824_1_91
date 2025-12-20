4b7f9daf1bf5515b20a4a1fa23c6de6552e22751
bala-amypo <bala@amypo.in>
2025-12-20 04:23:47 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRuleservice.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRuleservice.java
@@ -1,8 +1,8 @@
-package com.example.demo.service.impl;
-import com.example.demo.model.CategorizationRule;
+// package com.example.demo.service.impl;
+// import com.example.demo.model.CategorizationRule;
 
-public interface CategorizationRuleservice{
-    public CategorizationRule createRule(Long categoryId,CategorizationRule rule);
-    public List<CategorizationRule> getRulesByCategory(Long categoryId);
-    public CategorizationRule getRule(Long id);
-} 
+// public interface CategorizationRuleservice{
+//     public CategorizationRule createRule(Long categoryId,CategorizationRule rule);
+//     public List<CategorizationRule> getRulesByCategory(Long categoryId);
+//     public CategorizationRule getRule(Long id);
+// } 
--- src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
+++ src/main/java/com/example/demo/service/impl/UrgencyPolicyRulesimpl.java
@@ -1,20 +1,20 @@
-// import com.example.demo.model.UrgencyPolicy;
-// import com.example.demo.service.impl.UrgencyPolicyRules;
-// import com.example.demo.service.repository.UrgecyPolicyrepo;
-// import org.springframework.sterotype.Service;
-// import org.springframework.beans.factory.annotation.Autowired;
+import com.example.demo.model.UrgencyPolicy;
+import com.example.demo.service.impl.UrgencyPolicyRules;
+import com.example.demo.service.repository.UrgecyPolicyrepo;
+import org.springframework.sterotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
 
-// @Service
-// class UrgencyPolicyRulesimpl inplementation UrgencyPolicy{
-//     @Autowired
-//     UrgencyPolicy obj;
-//     public Urgencypolicy createPolicy(UrgencyPolicy policy){
-//         obj.save(policy);
-//     }
-//     public List<Urgencypolicy> getAllpolicies(){
-//         obj.findAll();
-//     }
-//     public Urgencypolicy getPolicy(Long id){
-//         obj.findById(id).orElse(null);
-//     }
-// }
+@Service
+class UrgencyPolicyRulesimpl inplementation UrgencyPolicy{
+    @Autowired
+    UrgencyPolicy obj;
+    public Urgencypolicy createPolicy(UrgencyPolicy policy){
+        obj.save(policy);
+    }
+    public List<Urgencypolicy> getAllpolicies(){
+        obj.findAll();
+    }
+    public Urgencypolicy getPolicy(Long id){
+        obj.findById(id).orElse(null);
+    }
+}
