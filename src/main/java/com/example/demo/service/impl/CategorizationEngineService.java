c09117f4475106ac8be582083eeaf5f255a67430
bala-amypo <bala@amypo.in>
2025-12-21 08:40:04 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationEngineService.java
+++ src/main/java/com/example/demo/service/impl/CategorizationEngineService.java
@@ -1,7 +1,8 @@
 package com.example.demo.service.impl;
 import java.util.List;
-
-
+import com.example.demo.model.Ticket;
+import com.example.demo.model.CategorizationLog;
 public interface CategorizationEngineService{
-    
+    Ticket categorizeTicket(Long ticketid);
+    List<CategorizationLog> getLogsForTicket()
 }
