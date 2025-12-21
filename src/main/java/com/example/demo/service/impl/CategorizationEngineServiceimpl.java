ebc009e890a004c78f0ab986ae175c5b6a8f37b3
bala-amypo <bala@amypo.in>
2025-12-21 08:49:03 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
@@ -3,6 +3,10 @@ package com.example.demo.service.impl;
 import com.example.demo.service.impl.CategorizationEngineService;
 import com.example.demo.model.Ticket;
 import com.example.demo.model.CategorizationLog;
+import com.example.demo.repository.Ticketrepo;
+import com.example.demo.repository.CategorizationLogrepo;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
-public CategorizationEngineServiceimpl implements Cate
+public class  CategorizationEngineServiceimpl implements CategorizationEngineService{
+    
+}
