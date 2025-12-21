27411a661d5cea6cd9f343765c726acb9267734c
bala-amypo <bala@amypo.in>
2025-12-21 04:57:31 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationRuleController.java
+++ src/main/java/com/example/demo/controller/CategorizationRuleController.java
@@ -6,10 +6,16 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import java.util.List;
 
 @RestController
 class CategrizationRuleController{
     @Autowired
     CategorizationRuleservice obj;
-    
+    CategorizationRule CreateRule(Long CategoryId,CategorizationRule rule){
+        return createRule(CategoryId,rule);
+    }
+    List<CategorizationRule> GetRule(){
+        return 
+    }
 }
