3c26eb7f2bb63f2b91b34199e562e2c83aef9545
bala-amypo <bala@amypo.in>
2025-12-21 10:07:01 +0000

    User commit

--- src/main/java/com/example/demo/controller/CategorizationEnginecontroller.java
+++ src/main/java/com/example/demo/controller/CategorizationEnginecontroller.java
@@ -5,7 +5,11 @@ import com.example.demo.model.CategorizationLog;
 import com.example.demo.service.impl.CategorizationEngineService;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.GetMapping;
-import 
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestMapping;
 import java.util.List;
 
 @RestController
