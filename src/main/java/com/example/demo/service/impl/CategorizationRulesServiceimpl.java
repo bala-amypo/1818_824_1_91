b8dcb203a253b4be8697ef1aa8fd1bca82e6049d
bala-amypo <bala@amypo.in>
2025-12-21 04:30:30 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
@@ -1,7 +1,10 @@
 package com.example.demo.service.impl;
+import java.util.List;
+import com.example.demo.service.CategorizationRulerepo;
+import org.springframework.beans.factory.annotation.Autowired;
 class CategorizatrionRulesServiceimpl implements CtaegorizationRuleService{
     public CategorizationRule createRule(Long categoryId,CategorizationRule rule){
-        ret
+        return 
     }
     public List<CategorizationRule> getRulesByCategory(Long categoryId){
 
