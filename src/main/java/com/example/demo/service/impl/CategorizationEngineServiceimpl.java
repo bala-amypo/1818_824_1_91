c00d73084c503f45fb34325bc158a184898c74a3
bala-amypo <bala@amypo.in>
2025-12-21 08:46:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
@@ -1,4 +1,8 @@
 package com.example.demo.service.impl;
 
 import com.example.demo.service.impl.CategorizationEngineService;
-import co
+import com.example.demo.model.Ticket;
+import com.example.demo.model.CategorizationLog;
+import org.springframework.stereotype.Service;
+import org.springframework.beans.factory.annotation.Autowired;
+public CategorizationEngineServiceimpl implements Cate
