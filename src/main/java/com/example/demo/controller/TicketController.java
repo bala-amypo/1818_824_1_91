b2d6af03445451cd96e2b914e659cafff28e0e64
bala-amypo <bala@amypo.in>
2025-12-21 10:22:12 +0000

    User commit

--- src/main/java/com/example/demo/controller/TicketController.java
+++ src/main/java/com/example/demo/controller/TicketController.java
@@ -8,7 +8,9 @@ import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
 
+@RequestMapping("/tickets")
 @RestController
 public class TicketController{
     @Autowired
--- src/main/java/com/example/demo/controller/UrgencyPolicycontroller.java
+++ src/main/java/com/example/demo/controller/UrgencyPolicycontroller.java
@@ -1,28 +1,30 @@
-// package com.example.demo.controller;
-// import org.springframework.web.bind.annotation.RestController;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import java.util.List;
-// import com.example.demo.model.UrgencyPolicy;
-// import com.example.demo.service.impl.UrgencyPolicyservice;
-// import org.springframework.web.bind.annotation.GetMapping;
-// import org.springframework.web.bind.annotation.PostMapping;
-// import org.springframework.web.bind.annotation.RequestBody;
-// import org.springframework.web.bind.annotation.PathVariable;
-// import org.springframework.beans.factory.annotation.Autowired;
-// @RestController
-// public class UrgencyPolicycontroller{
-// @Autowired
-// UrgencyPolicyservice obj;
-// @PostMapping("/get")
-// public UrgencyPolicy theCreatePolicy(@RequestBody UrgencyPolicy policy){
-//     return obj.createPolicy(policy);
-// }
-// @GetMapping("/{id}")
-// public UrgencyPolicy getPolicy(@PathVariable Long id){
-//     return obj.getPolicy(id);
-// }
-// @GetMapping("/cat")
-// public List<UrgencyPolicy> getPol(){
-//     return obj.getAllpolicies();
-// }
-// }
+package com.example.demo.controller;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.bind.annotation.RequestBody;
+import java.util.List;
+import com.example.demo.model.UrgencyPolicy;
+import com.example.demo.service.impl.UrgencyPolicyservice;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.beans.factory.annotation.Autowired;
+@Reques
+@RestController
+public class UrgencyPolicycontroller{
+@Autowired
+UrgencyPolicyservice obj;
+@PostMapping("/get")
+public UrgencyPolicy theCreatePolicy(@RequestBody UrgencyPolicy policy){
+    return obj.createPolicy(policy);
+}
+@GetMapping("/{id}")
+public UrgencyPolicy getPolicy(@PathVariable Long id){
+    return obj.getPolicy(id);
+}
+@GetMapping("/cat")
+public List<UrgencyPolicy> getPol(){
+    return obj.getAllpolicies();
+}
+}
