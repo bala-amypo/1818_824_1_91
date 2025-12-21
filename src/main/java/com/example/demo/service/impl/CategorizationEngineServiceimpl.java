023039fa53651783f5c1afc185fcf76f369bfbe1
bala-amypo <bala@amypo.in>
2025-12-21 08:52:01 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationEngineServiceimpl.java
@@ -8,5 +8,9 @@ import com.example.demo.repository.CategorizationLogrepo;
 import org.springframework.stereotype.Service;
 import org.springframework.beans.factory.annotation.Autowired;
 public class  CategorizationEngineServiceimpl implements CategorizationEngineService{
-    
+    @Autowired
+    Ticketrepo obj
+    @Autowired
+    CategorizationLogrepo ob;
+    public CategorizationRule createRule(Long categoryId,CategorizationRule rule)
 }
