e73897e18b72b24d2ffab176c4ebc9195bc50da0
bala-amypo <bala@amypo.in>
2025-12-21 06:39:31 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
@@ -1,15 +1,16 @@
 package com.example.demo.service.impl;
 import java.util.List;
 import com.example.demo.service.CategorizationRulerepo;
-import com.example.demo.sevice.Category;
+import com.example.demo.repository.Categoryrepo;
 import org.springframework.beans.factory.annotation.Autowired;
 class CategorizatrionRulesServiceimpl implements CtaegorizationRuleService{
     @Autowired
     CategorizationRulerepo obj;
+    Categoryrepo ob;
     public CategorizationRule createRule(Long categoryId,CategorizationRule rule){
         Category category=findById(categoryId)
-            .orElse(throw->newRunTi)
-        rule.category;
+            .orElseThrow(()->new RunTimeException("No Id Found"));
+        rule.setCategory(category);
 
     }
     public List<CategorizationRule> getRulesByCategory(Long categoryId){
