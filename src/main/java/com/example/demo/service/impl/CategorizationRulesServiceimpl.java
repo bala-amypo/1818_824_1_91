0da3500aa73d1d5196ffd18787f60ac066dcc46c
bala-amypo <bala@amypo.in>
2025-12-21 04:27:31 +0000

    User commit

--- src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
+++ src/main/java/com/example/demo/service/impl/CategorizationRulesServiceimpl.java
@@ -1,12 +1,12 @@
-// package com.example.demo.service.impl;
-// class CategorizatrionRulesServiceimpl implements CtaegorizationRuleService{
-//     public CategorizationRule createRule(Long categoryId,CategorizationRule rule){
-        
-//     }
-//     public List<CategorizationRule> getRulesByCategory(Long categoryId){
+package com.example.demo.service.impl;
+class CategorizatrionRulesServiceimpl implements CtaegorizationRuleService{
+    public CategorizationRule createRule(Long categoryId,CategorizationRule rule){
+        ret
+    }
+    public List<CategorizationRule> getRulesByCategory(Long categoryId){
 
-//     }
-//     public CategorizationRule getRule(Long id){
+    }
+    public CategorizationRule getRule(Long id){
 
-//     }
-// }
+    }
+}
