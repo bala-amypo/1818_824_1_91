dada9ff2ef5801ecf3b265bb911888bb88cc9f7c
bala-amypo <bala@amypo.in>
2025-12-23 16:51:52 +0000

    User commit

--- src/main/java/com/example/demo/controller/UserController.java
+++ src/main/java/com/example/demo/controller/UserController.java
@@ -9,7 +9,7 @@ import org.springframework.web.bind.annotation.PostMapping;
 import org.springframework.web.bind.annotation.PutMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.PatchMapping;
-import org.springframework.wen.bind.annotation.DeleteMapping;
+import org.springframework.web.bind.annotation.DeleteMapping;
 import java.util.List;
 import org.springframework.web.bind.annotation.RequestMapping; 
 @RequestMapping("/users")
@@ -38,7 +38,7 @@ public class UserController{
             return obj.UpateMin(id,data);
         }
     @DeleteMapping("/delete/{Abi}")
-    public User deleteuser(@PathVariable string Abi){
+    public User deleteuser(@PathVariable String Abi){
         return obj.deleteUser(Abi);
     }
 }
