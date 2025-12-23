3006e547c8d2db558906c45aff00d75881e914ac
bala-amypo <bala@amypo.in>
2025-12-23 16:33:51 +0000

    User commit

--- src/main/java/com/example/demo/repository/Userrepo.java
+++ src/main/java/com/example/demo/repository/Userrepo.java
@@ -2,11 +2,12 @@ package com.example.demo.repository;
 import com.example.demo.model.User;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.data.jpa.repository.Query;
+import org.springframework.data.repository.query.Param;
 import java.util.Optional;
 public interface Userrepo extends JpaRepository<User,Long>{
     Optional<User> findByEmail(String Email);
-    @Query("SELECT n FROM USER n n.name")
-    User getByName(@RequestParam String Name){
+    @Query("SELECT n FROM USER n n.name ")
+    User getByName(@Param String Name){
 
     }
 }
