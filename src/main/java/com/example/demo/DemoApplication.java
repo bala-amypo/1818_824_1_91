e7f877f0e7141daddb5822eb6cb11211c00c0c22
bala-amypo <bala@amypo.in>
2025-12-25 04:00:24 +0000

    User commit

--- src/main/java/com/example/demo/DemoApplication.java
+++ src/main/java/com/example/demo/DemoApplication.java
@@ -2,13 +2,13 @@ package com.example.demo;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.crypto.bcrypt.BcryptPasswordEncoder;
+import org.springframework.security.crypto.bcrypt.BcryptPasswordEncoder;
 @SpringBootApplication
 public class DemoApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(DemoApplication.class, args);
-		Systemt.out.println(BcryptPasswordEncoder().encode("123456"));
+		System.out.println(BcryptPasswordEncoder().encode("123456"));
 	}
 
 }
